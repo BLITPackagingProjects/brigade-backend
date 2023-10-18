3001c3560595f2d82f45a38150c0cb85450a6e69
Shyam Sharma <shyamsharma.sharma797@gmail.com>
2023-10-18 11:31:56 -0400

    Commit

---
+++ src/main/java/com/brigadeApp/petAdoption/Controller/ApplicationController.java
@@ -0,0 +1,27 @@
+package com.brigadeApp.petAdoption.Controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.brigadeApp.petAdoption.Entity.Application;
+import com.brigadeApp.petAdoption.Service.ApplicationService;
+
+@RestController
+@RequestMapping("/application")
+public class ApplicationController {
+
+    @Autowired
+    ApplicationService applicationService;
+
+    @GetMapping("/{id}")
+    public List<Application> getAllApplicationsForUser(@PathVariable("id") int id) {
+        return applicationService.showListOfAppFor(id);
+
+    }
+
+}
